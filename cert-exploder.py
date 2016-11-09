c204afce043ff2944599a09d3fdd1d9a055a6b96
john-veillette <johnv@uchicago.edu>
2016-11-09 11:52:17 -0600

    Made a fix where I was calling a whole column instead of a value, still getting an error when executing line 19

--- cert-exploder.py
+++ cert-exploder.py
@@ -21,7 +21,7 @@ def explode(filepath):
 	endorsements = pd.DataFrame(columns = ['teacherID', 'endorsement'])
 	for row in range(len(teachers)):
 		ID = teachers['teacherID'][row]
-		endor = teachers['ENDORSMENT'] # "endorsement" is mispelled in the dataset
+		endor = teachers['ENDORSMENT'][row] # "endorsement" is mispelled in the dataset
 		endor = endor.split('|')
 		for e in endor:
 			df = pd.DataFrame([ID, e], columns = ['teacherID', 'endorsement'])
@@ -53,15 +53,18 @@ def main(filepath):
 	for row in range(len(certifications)):
 		c = certifications['certification'][row]
 		ct_certifications[c] = ct_certifications.get(c, 0) + 1
-	with open('reports/certification_counts.csv', 'wb') as f: 
-        w = csv.DictWriter(f, ['certification', 'count'])
-        w.writeheader()
-        for key in ct_certifications:
-            w.writerow([key, ct_certifications[key]])
-    #make a .csv for each type of certifcation
-    for key in ct_certifications:
-    	certs = certifications[certifications['certification'] == key]
-    	IDs = list(certs.pop('teacherID'))
-    	twc = teachers[teachers['teacherID'] in IDs] #teachers w certification in question
-    	del twc['teacherID']
-    	twc.to_csv('reports/certifications/' + key + ".csv")
+	with open('reports/certification_counts.csv', 'wb') as f:
+		w = csv.DictWriter(f, ['certification', 'count'])
+		w.writeheader()
+		for key in ct_certifications:
+			w.writerow([key, ct_certifications[key]])
+	#make a file for each ceetification of teachers with that certification
+	for key in ct_certifications:
+		certs = certifications[certifications['certification'] == key]
+		IDs = list(certs.pop('teacherID'))
+		twc = teachers[teachers['teacherID'] in IDs] #teachers w certification in question
+		del twc['teacherID']
+		wc.to_csv('reports/certifications/' + key + ".csv")
+
+if __name__ == '__main__':
+	main("Comp Sci Licenses.xlsx")
