6afc36385856f3bd16a3b36204f8bc494bfdf420
john-veillette <johnv@uchicago.edu>
2016-11-14 16:55:34 -0600

    Fixed remaining bugs

--- cert-exploder.py
+++ cert-exploder.py
@@ -19,22 +19,24 @@ def explode(filepath):
 		teachers.set_value(row, 'teacherID', row) #teacherID = row in og dataframe
 	#create dataframe that records endorsements
 	endorsements = pd.DataFrame(columns = ['teacherID', 'endorsement'])
+	index = 0
 	for row in range(len(teachers)):
 		ID = teachers['teacherID'][row]
 		endor = str(teachers['ENDORSMENT'][row]) # "endorsement" is mispelled in the dataset
 		endor = endor.split('|')
 		for e in endor:
-			df = pd.DataFrame.from_dict({'teacherID': [ID], 'endorsement': [e]}, orient = 'columns')
-			endorsements = endorsements.append(df)
+			endorsements.loc[index] = [ID, e]
+			index += 1
 	#create dataframe that records certifications
 	certifications = pd.DataFrame(columns = ['teacherID', 'certification'])
+	index = 0
 	for row in range(len(teachers)):
 		ID = teachers['teacherID'][row]
-		cert = str(teachers['CERTIFICATION'])
+		cert = str(teachers['CERTIFICATION'][row])
 		cert = cert.split('|')
 		for c in cert:
-			df = pd.DataFrame([ID, c], columns = ['teacherID', 'certification'])
-			certifications = certifications.append(df)
+			certifications.loc[index] = [ID, c]
+			index += 1
 	#remove 'ENDORSMENT' and 'CERTIFICATION' from teachers dataframe
 	del teachers['ENDORSMENT']
 	del teachers['CERTIFICATION']
@@ -42,10 +44,6 @@ def explode(filepath):
 	return endorsements, certifications, teachers
 
 def main(filepath):
-	'''
-	Makes two csv files from the given Excel report with fields 'ENDORSMENT' and 
-	'CERTIFICATION', one for endorsements and one for ceritifcations
-	'''
 	#make a .csv with columns for each certification and number of
 	#	teachers with that certification
 	endorsements, certifications, teachers = explode(filepath)
@@ -53,18 +51,18 @@ def main(filepath):
 	for row in range(len(certifications)):
 		c = certifications['certification'][row]
 		ct_certifications[c] = ct_certifications.get(c, 0) + 1
-	with open('reports/certification_counts.csv', 'wb') as f:
-		w = csv.DictWriter(f, ['certification', 'count'])
-		w.writeheader()
+	with open('reports/certification_counts.csv', 'w') as f:
+		w = csv.writer(f)
+		w.writerow(['Certification', 'Count'])
 		for key in ct_certifications:
 			w.writerow([key, ct_certifications[key]])
-	#make a file for each ceetification of teachers with that certification
+	#make a file for each certification of teachers with that certification
 	for key in ct_certifications:
 		certs = certifications[certifications['certification'] == key]
 		IDs = list(certs.pop('teacherID'))
-		twc = teachers[teachers['teacherID'] in IDs] #teachers w certification in question
+		twc = teachers[teachers['teacherID'].isin(IDs)] #teachers w certification in question
 		del twc['teacherID']
-		wc.to_csv('reports/certifications/' + key + ".csv")
+		twc.to_csv('reports/certifications/' + key + ".csv")
 
 if __name__ == '__main__':
 	main("Comp Sci Licenses.xlsx")
